c4de6285a03a55d0540d2616818e4dfc2857a44c
Craig Morris <cr@igmorr.is>
2017-10-27 14:09:13 +0100

    Migrations working

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,8 +1,21 @@
 var SafeMath = artifacts.require("./SafeMath.sol");
+var Pausable = artifacts.require("./Pausable.sol");
+var Ownable = artifacts.require("./Ownable.sol");
+var Destructible = artifacts.require("./Destructible.sol");
+
+var Sale = artifacts.require("./Sale.sol");
 var Token = artifacts.require("./Token.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SafeMath);
-  deployer.link(SafeMath, Token);
-  deployer.deploy(Token);
+module.exports = async function(deployer) {
+
+  deployer.deploy([SafeMath, Pausable, Ownable, Destructible]);
+
+  deployer.link(SafeMath, [Token, Sale]);
+  deployer.link(Pausable, Sale);
+  deployer.link(Ownable, Sale);
+  deployer.link(Destructible, Sale);
+  deployer.link(Token, Sale);
+
+  deployer.then(() => Token.new())
+          .then(instance => Sale.new(instance.address))
 };
