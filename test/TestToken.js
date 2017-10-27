67df74ccdf3c4b5e45766f86be236dcb5030d11c
Craig Morris <cr@igmorr.is>
2017-10-27 11:36:20 +0100

    Working JS test

---
+++ test/TestToken.js
@@ -0,0 +1,12 @@
+var Token = artifacts.require("./Token.sol");
+
+contract('Token', function(accounts) {
+
+  it('should put 1M ITM in the first account', async function() {
+    let instance = await Token.deployed()
+    let balance = await instance.balanceOf.call(accounts[0]);
+
+    assert.equal(balance.valueOf(), 100000000, "100000000 wasn't in the first account");
+  });
+
+});
