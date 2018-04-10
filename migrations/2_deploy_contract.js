4d389a33d3043d6468f8dea879c3c15c664c9f67
Craig Morris <cr@igmorr.is>
2018-04-10 16:13:19 +0100

    wip

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -7,12 +7,12 @@ module.exports = async function(deployer, network, accounts) {
 
   await deployer.deploy(
     Sale,
-    0, // uint256 _timeFromNow, 
-    1209600, // uint256 _duration, 
-    1, // uint256 _rate, 
+    1523372145, // uint256 _startTime, 04/10/2018 @ 2:55pm
+    3628800, // uint256 _duration, 6 weeks
+    600, // uint256 _rate, 600 per ETH
     accounts[0], // address _wallet_address, 
     Token.address, // address _token_address, 
-    10, // uint256 _cap,
-    1, // uint8 _round
+    '5e25', // uint256 _cap,
+    0, // uint8 _round
   )
 };
