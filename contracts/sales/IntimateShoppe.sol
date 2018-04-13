0ad2409d05bd0ce15f23a18e225fc79fe25bfda5
Craig Morris <cr@igmorr.is>
2018-04-13 16:50:30 +0100

    Allow setRate during sale

--- contracts/sales/IntimateShoppe.sol
+++ contracts/sales/IntimateShoppe.sol
@@ -188,8 +188,6 @@ contract IntimateShoppe is Pausable, RecoverCurrency {
     /// @dev Set the rate, i.e. how many units per wei do we give
     /// @param _rate How many token units are offered for 1 wei, 1 or more.
     function setRate(uint256 _rate) public ifAuthorized(msg.sender, APHRODITE) {
-        /// Cannot be modified once sale is ongoing
-        require(now < startTime || now > endTime);
         require(_rate > 0);
         rate = _rate;
     }
