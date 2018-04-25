fd81c25ff2d98a92cb3998d09a94c1cbb08afb4c
Craig Morris <cr@igmorr.is>
2018-04-25 17:50:57 +0100

    separate sale and shop contracts

---
+++ migrations/3_deploy_sale.js
@@ -0,0 +1,24 @@
+var Token = artifacts.require("./Aphrodite.sol")
+var Sale = artifacts.require("./IntimateShoppe.sol")
+let networks = require('../truffle').networks
+const sha3 = require('solidity-sha3').default
+
+const APHRODITE = sha3('Goddess of Love!')
+
+module.exports = async function(deployer, network, accounts) {
+  
+  await deployer.deploy(
+    Sale,
+    1524733200, // uint256 _startTime, 04/26/2018 @ 9:00am
+    3628800, // uint256 _duration, 6 weeks
+    600, // uint256 _rate, 600 per ETH
+    accounts[0], // address _wallet_address, 
+    Token.address, // address _token_address, 
+    '594730524e18', // uint256 _cap, 600M - 5,269,476 sold in presale
+    0, // uint8 _round
+  )
+
+  let token = await Token.deployed()
+  await token.approve(Sale.address, '594730524e18');
+  await token.toggleAuthorization(Sale.address, APHRODITE)
+};
